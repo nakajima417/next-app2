3a719540e81374b84bf37295cf0bea119e5fb8e5
Daisuke Nakajima <84059831+nakajima417@users.noreply.github.com>
2023-10-28 12:14:31 +0000

    追記

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,16 +1,45 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 //SSGの場合
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
+    const req = await fetch('http://localhost:3000/${params.id}.json');
+    const data = await req.json();
 
-    
+    return {
+        props: {
+            product: data,
+        },
+    };
+
 }
 
-const Product = () => {
+export async function getStaticPaths() {
+    const req = await fetch('http://localhost:3000/${params.id}.json');
+    const data = await req.json();
+
+    return {
+        props: {
+            product: data,
+        },
+    };
+}
+
+const Product = ({ product }) => {
     const router = useRouter();
     console.log(router.query.id);
     const { id } = router.query;
-    return <div>任意のID</div>;
+    return <div className={styles.container} >
+        <main className={styles.main}>
+            <h1>{id}のページです</h1>
+            <img src={product.image} width="300" height="400" />
+            <p>{product.name}</p>
+            <br />
+            <Link href="/products">
+                <a>商品一覧へ</a>
+            </Link>
+        </main>
+    </div>;
 };
 
 export default Product;
